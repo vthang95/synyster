2f6b458a1bc4f06a6e2a19b81bc4d18235a344c0
vthang95 <tranvietthang95@gmail.com>
2018-02-26 01:33:41 +0700

    Fix layout

--- pages/about.js
+++ pages/about.js
@@ -19,18 +19,17 @@ class AboutPage extends Component {
         <div className="syn-bio">
           Xin chào, <br /><br />
 
-          Mình là Thắng. Mình xuất thân từ một người chuyên tự nhiên vì thế nên văn mình khá kém.
-          Chính vì thế mình đã tạo blog này để note lại những gì mình đã nghiên cứu được để hiểu sâu vấn đề hơn.
+          Mình là Thắng. Mình xuất thân từ một người chuyên tự nhiên vì thế văn mình khá kém.
+          Vậy nên mình đã tạo blog này để tập viết, tập note lại những gì mình đã nghiên cứu được để hiểu sâu vấn đề hơn.
           Hay chỉ đơn giản là chia sẻ những câu chuyện thú vị. <br /><br />
 
           Đối với mình việc chia sẻ kiến thức là một điều rất có ý nghĩa.
           Do mình học trái ngành (kinh tế) và mới chuyển sang software development nên việc tự học là điều tất yếu
           phải làm. Hầu hết các resources mình học qua là các tuts, các bài viết được chia sẻ trên mạng.
-          Nên mình cũng rất mong muốn được chia sẻ kiến thức của mình đến mọi người trong điều kiện thời
-          gian cho phép.<br /><br />
+          Nên mình cũng rất mong muốn được chia sẻ kiến thức của mình đến tất mọi người.<br /><br />
 
-          Và tất nhiên với một newbie như mình thì việc hiểu sai vấn đề là chuyện khá bình thường.
-          Mình còn trẻ trâu nữa nên khó tránh khỏi sai lầm.
+          Và tất nhiên với một newbie như mình thì việc hiểu sai vấn đề là chuyện bình thường.
+          Mình còn trẻ trâu nữa nên khó tránh khỏi những sai lầm. :))
           Vì vậy mình xin nhất tất cả gạch đá yêu thương từ mọi người :D<br /><br />
 
           Mong mọi người ủng hộ :))
