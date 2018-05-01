d30563de617f0b3eee30f10e4ed990d1adb5e6f6
vthang95 <tranvietthang95@gmail.com>
2018-05-01 22:32:28 +0700

    Use stage-2 instead of stage-0

--- pages/manage/post-management/index.js
+++ pages/manage/post-management/index.js
@@ -2,7 +2,7 @@ import { Component } from "react"
 import Link from "next/link"
 import Router from "next/router"
 import Notification from "components/Notification"
-import { sendPost } from "utils/request"
+import { sendPost, sendGet } from "utils/request"
 
 import { pageWrapper } from "utils/wrapper"
 
@@ -12,14 +12,40 @@ class ManagePostPage extends Component {
 
     this.state = {
       title: "",
-      content: ""
+      content: "",
+      posts: []
     }
   }
 
-  handleCreateNewPost = () => {
-    sendPost("/api/posts", null, this.state)
-      .then(res => console.log(res))
-      .catch(err => console.log(err))
+  componentDidMount() {
+    sendGet("/api/posts")
+      .then(res => {
+        if (res.status === 200 && res.data.success === true) {
+          this.setState({ posts: res.data.docs })
+        } 
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  handleEditPost = (el) => {
+    return function() {
+      
+    }
+  }
+
+  renderPosts() {
+    const posts = this.state.posts
+    if (!Array.isArray(posts) || posts.length === 0) return
+    return posts.map((el, idx) => {
+      return (
+        <li key={idx} style={{ display: "flex", justifyContent: "space-between" }}>
+          <span>{el.title}</span>
+          <span onClick={this.handleEditPost(el)}><a>Edit</a></span>
+        </li>
+      )
+    })
   }
 
   render() {
@@ -32,6 +58,10 @@ class ManagePostPage extends Component {
         <Link href="/manage/post-management/new">
           <a><h3>Create new post</h3></a>
         </Link>
+
+        <ul>
+          {this.renderPosts()}
+        </ul>
       </div>
     )
   }
